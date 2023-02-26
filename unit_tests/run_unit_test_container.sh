92453a81b41f9e5ac723b3fdf1381a983bbb8330
JONG KIM <jong.kim@noaa.gov>
2023-02-26 17:18:19 -0500

    Update run_unit_test_container.sh

--- unit_tests/run_unit_test_container.sh
+++ unit_tests/run_unit_test_container.sh
@@ -11,4 +11,19 @@ fi
 /bin/sh -c /home/builder/stochastic_physics/unit_tests/build_standalone.sh
 cp -r /tmp/INPUT /home/builder/stochastic_physics/unit_tests/
 echo $(ls)
-echo $(which sed)
+mkdir -p RESTART
+
+# test 3 different domain decompositions and compare to baseline
+#layout 1x4
+#cp input.nml.template input.nml
+sed -i -e "s/LOX/1/g" input.nml
+sed -i -e "s/LOY/4/g" input.nml
+sed -i -e "s/NPX/$NPX/g" input.nml
+sed -i -e "s/NPY/$NPY/g" input.nml
+sed -i -e "s/RES/$RES/g" input.nml
+sed -i -e "s/_STOCHINI_/.false./g" input.nml
+export OMP_NUM_THREADS=2
+module list
+time srun --label -n 24 standalone_stochy.x
+mkdir stochy_out
+mv workg* stochy_out
