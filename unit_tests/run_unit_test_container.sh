9cef06d121abc44501008e1972d7858fbf897704
JONG KIM <jong.kim@noaa.gov>
2023-02-26 16:48:32 -0500

    Update run_unit_test_container.sh

--- unit_tests/run_unit_test_container.sh
+++ unit_tests/run_unit_test_container.sh
@@ -3,11 +3,11 @@ RES=96
 NPX=`expr $RES + 1`
 NPY=`expr $RES + 1`
 HOME=/home/builder
-cd $HOME/stochastic_physics/unit_tests
-if [ ! -f build_standalone.sh ];then
+cd /home/builder/stochastic_physics/unit_tests
+if [ ! -f /home/builder/stochastic_physics/unit_tests/build_standalone.sh ];then
   echo "No build script! check build_standalone.sh in container"
   exit 1
 fi
-/bin/sh -c build_standalone.sh
-cp /tmp/INPUT .
+/bin/sh -c /home/builder/stochastic_physics/unit_tests/build_standalone.sh
+cp -r /tmp/INPUT .
 echo $(ls -l)
